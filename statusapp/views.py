7d7d49322df49ea2fb829e2ce50bc70dd3e7f0f1
Sabinbhattarai <sabin.bhattarai2012@gmail.com>
2021-03-02 17:39:46 +0545

    Some modifications

--- statusapp/views.py
+++ statusapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic import CreateView
+from .forms import StatusMessageModelForm
+
 # Create your views here.
+
+class StatusMessageCreateView(CreateView):
+    form_class = StatusMessageModelForm
+    template_name = 
